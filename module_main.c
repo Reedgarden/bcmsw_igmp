d59b4d80576974391b726fdaf7a0921bc47bec7a
juooh <juooh@naver.com>
2012-05-18 22:28:16 +0900

    tasklet

--- module_main.c
+++ module_main.c
@@ -8,6 +8,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 
 #include "bcmsw_mii.h"
 #include "bcmsw_igmp.h"
@@ -15,6 +16,10 @@
 //schedule
 static void tasklet_start(void);
 void ethsw_igmp_peek(unsigned long node);
+void delay_loop(unsigned long t);
+
+char my_tasklet_data[]="my_tasklet_function was called";
+DECLARE_TASKLET(peek_igmp_send, ethsw_igmp_peek, (unsigned long)&my_tasklet_data);
 
 static int init_bcmsw_module(void)
 {
@@ -23,9 +28,10 @@ static int init_bcmsw_module(void)
 	dev = net_get_device();
 	net_dev_test_mii_rw(dev);
 
+	tasklet_start();
+
 	igmp_wrap_init();
 
-	tasklet_start();
 	return 0;
 }
 
@@ -33,19 +39,28 @@ static void deinit_bcmsw_module(void)
 {
 	printk("deinit\n");
 	igmp_wrap_deinit();
+
+	tasklet_kill(&peek_igmp_send);
 }
 
 static void tasklet_start(void)
 {
-	DECLARE_TASKLET(peek_igmp_send, ethsw_igmp_peek, NULL);
+
+	tasklet_schedule(&peek_igmp_send);
 }
 void ethsw_igmp_peek(unsigned long node)
 {
-	while(1)
-	{
-		printk("moduel ... \n");
-		msleep(1000);
-	}
+//	while(1)
+//	{
+		printk("moduel ... %s \n", (char*)node);
+//		delay_loop(1);
+//	}
+}
+
+void delay_loop(unsigned long t)
+{
+	unsigned long delay = jiffies + t * HZ;
+	while( time_before( jiffies, delay ));
 }
 
 module_init( init_bcmsw_module );
